50cb91dc4ff8f226c592fa39951dbc118e08c436
Emilio Pavia <emilio.pavia@gmail.com>
2021-11-10 19:38:23 +0100

    Initial commit

---
+++ Playground.playground/Contents.swift
@@ -0,0 +1,7 @@
+import CodeTest
+
+["a", "b", "c"].numberOfItemsWithRepeatedChar
+["a", "b", "cc"].numberOfItemsWithRepeatedChar
+["abc", "deef", "hi", "mnmn"].numberOfItemsWithRepeatedChar
+
+[[1, 2, [3]], 4].flatten() 
